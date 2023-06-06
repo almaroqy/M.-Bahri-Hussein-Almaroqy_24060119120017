d7192fefa867de501256a7faa0bf125c59aed591
almaroqy <91356466+almaroqy@users.noreply.github.com>
2023-06-06 20:25:31 +0700

    Add files via upload

---
+++ Praktikum 9/PersonDAO.java
@@ -0,0 +1,9 @@
+/*
+	File: PersonDAO.java
+	Nama: M. Bahri Hussein Almaroqy
+	Deskripsi: interface untuk person access object
+*/
+
+public interface PersonDAO{
+	public void savePerson(Person p) throws Exception;
+}
